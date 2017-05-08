e42be117aa9af8e52508c8d4433dc29f5879d86e
Ben Lu <ben@ben-l.com>
2017-05-08 22:46:34 +1200

    Wrote some tests ideas

--- api/src/containers/Home.test.js
+++ api/src/containers/Home.test.js
@@ -7,32 +7,34 @@ import Home from './Home';
 //  ReactDOM.render(<Home store={{}} />, div);
 //});
 
-describe('auth', ()=>{
-  it('has login', ()=>{ expect(false).toBeTruthy() })
-  it('has logout', ()=>{ expect(false).toBeTruthy() })
-  it('has roles', ()=>{ expect(false).toBeTruthy() })
-  it('has register', ()=>{ expect(false).toBeTruthy() })
-  it('has forgotpassword', ()=>{ expect(false).toBeTruthy() })
+describe('layout', ()=>{
+  it('has gameboard', ()=>{ expect(false).toBeTruthy() })
+  it('has players sections', ()=>{ expect(false).toBeTruthy() })
+  it('has rooms', ()=>{ expect(false).toBeTruthy() })
+  it('has socket.io', ()=>{ expect(false).toBeTruthy() })
+  it('has player naming', ()=>{ expect(false).toBeTruthy() })
+  it('has player finding', ()=>{ expect(false).toBeTruthy() })
+  it('has players page', ()=>{ expect(false).toBeTruthy() })
+  it('has game page', ()=>{ expect(false).toBeTruthy() })
+  it('has victory page/overlay', ()=>{ expect(false).toBeTruthy() })
+  it('has loser page/overlay', ()=>{ expect(false).toBeTruthy() })
 })
-describe('subscriptions', () => {
-  it('has free state', ()=>{ expect(false).toBeTruthy() })
-  it('has paid tier', ()=>{ expect(false).toBeTruthy() })
-  it('can take real credit cards', ()=>{ expect(false).toBeTruthy() })
-  it('has user logins', ()=>{ expect(false).toBeTruthy() })
+describe('keyboard shortcuts', ()=>{
+  it('cursor movement', ()=>{ expect(false).toBeTruthy() })
+  it('dice roll', ()=>{ expect(false).toBeTruthy() })
+  it('piece movement', ()=>{ expect(false).toBeTruthy() })
 })
-describe('layout', () => {
-  it('has setup snapshot testing', ()=>{ expect(true).toBeTruthy() })
-  it('has a frontpage', ()=>{ expect(false).toBeTruthy() })
-  it('has a login', ()=>{ expect(false).toBeTruthy() })
-  it('has a registration', ()=>{ expect(false).toBeTruthy() })
-  it('has an event page', ()=>{ expect(false).toBeTruthy() })
+describe('backend', ()=>{
+  it('saves game state - every move', ()=>{ expect(false).toBeTruthy() })
+  it('restore game state when opening a room', ()=>{ expect(false).toBeTruthy() })
+  it('validate via move count - should always be sequential', ()=>{ expect(false).toBeTruthy() })
+  it('in a room, live updates with socket io', ()=>{ expect(false).toBeTruthy() })
 })
-describe('backend', () => {
-  it('setup CI', ()=>{ expect(false).toBeTruthy() })
-})
-describe('events', ()=>{
-  it('can add people to events', ()=>{ expect(false).toBeTruthy() })
-  it('comments', ()=>{ expect(false).toBeTruthy() })
-  it('description', ()=>{ expect(false).toBeTruthy() })
-  it('schedule', ()=>{ expect(false).toBeTruthy() })
+describe('gameplay', ()=>{
+  it('pieces and their movement', ()=>{ expect(false).toBeTruthy() })
+  it('dice roll - 4 tetrahedral with 2 white points each', ()=>{ expect(false).toBeTruthy() })
+  it('has squares for re rolls', ()=>{ expect(false).toBeTruthy() })
+  it('has invulnerable square with reroll', ()=>{ expect(false).toBeTruthy() })
+  it('has a show valid moves', ()=>{ expect(false).toBeTruthy() })
+  it('removes enemy piece', ()=>{ expect(false).toBeTruthy() })
 })
