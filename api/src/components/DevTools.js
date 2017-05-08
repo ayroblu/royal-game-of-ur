e1ce04f9f24b09e9875aa044c489eadf6c30712f
Ben Lu <ben@ben-l.com>
2017-05-08 22:15:38 +1200

    Added back dev tools with correct spelling

---
+++ api/src/components/DevTools.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {createDevTools} from 'redux-devtools'
+import LogMonitor from 'redux-devtools-log-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+
+/**
+ * Create the DevTools component and export it.
+ */
+export default createDevTools(
+  <DockMonitor
+    toggleVisibilityKey='ctrl-h'
+    changePositionKey='ctrl-q'
+    defaultIsVisible={false}
+  >
+    <LogMonitor theme='tomorrow' />
+  </DockMonitor>
+)
